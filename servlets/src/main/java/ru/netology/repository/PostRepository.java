bdf9097e4d87d0c10b0069a5904633cfb0590113
imLIVI <lesly120898@gmail.com>
2023-03-07 00:56:18 +0300

    Implement Annotation Config

--- servlets/src/main/java/ru/netology/repository/PostRepository.java
+++ servlets/src/main/java/ru/netology/repository/PostRepository.java
@@ -1,5 +1,6 @@
 package ru.netology.repository;
 
+import org.springframework.stereotype.Repository;
 import ru.netology.model.Post;
 
 import java.util.ArrayList;
@@ -8,6 +9,7 @@ import java.util.List;
 import java.util.Optional;
 
 // Stub
+@Repository
 public class PostRepository {
 
     private final List<Post> postsList = Collections.synchronizedList(new ArrayList<>());
