9b0aed7e0c2d8318e9a0557ff5a926d3cbfc6da7
imLIVI <lesly120898@gmail.com>
2023-03-06 13:52:20 +0300

    Implement all of methods

--- servlets/src/main/java/ru/netology/repository/PostRepository.java
+++ servlets/src/main/java/ru/netology/repository/PostRepository.java
@@ -10,24 +10,44 @@ import java.util.Optional;
 // Stub
 public class PostRepository {
 
-  private final List<Post> postsList = Collections.synchronizedList(new ArrayList<>());
-  private int counter = 1;
-  public List<Post> all() {
-    return postsList;
-  }
+    private final List<Post> postsList = Collections.synchronizedList(new ArrayList<>());
+    private int counter = 1;
 
-  public Optional<Post> getById(long id) {
-    Post post = postsList.stream()
-            .filter(p -> p.getId() == id)
-            .findFirst()
-            .orElse(null);
-    return (post == null) ? Optional.empty() : Optional.of(post);
-  }
+    public List<Post> all() {
+        return postsList;
+    }
 
-  public Post save(Post post) {
-    return post;
-  }
+    public Optional<Post> getById(long id) {
+        Post post = findById(id);
+        return (post == null) ? Optional.empty() : Optional.of(post);
+    }
 
-  public void removeById(long id) {
-  }
+    public Post save(Post post) {
+        if (post.getId() == 0) {
+            post.setId(counter);
+            counter++;
+            postsList.add(post);
+            return post;
+        } else {
+            Post searchingPost = findById(post.getId());
+            if (searchingPost != null) {
+                postsList.set(postsList.indexOf(searchingPost), post);
+            }
+        }
+        return post;
+    }
+
+    public void removeById(long id) {
+        Post searchingPost = findById(id);
+        if (searchingPost != null) {
+            postsList.remove(searchingPost);
+        }
+    }
+
+    public Post findById(long id) {
+        return postsList.stream()
+                .filter(p -> p.getId() == id)
+                .findFirst()
+                .orElse(null);
+    }
 }
