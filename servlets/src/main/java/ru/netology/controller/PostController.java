291d1af2f13885a9dd5e7e865fece1c2e62abac2
imLIVI <lesly120898@gmail.com>
2023-03-06 13:50:12 +0300

    Implement deserialize request & serialize response

--- servlets/src/main/java/ru/netology/controller/PostController.java
+++ servlets/src/main/java/ru/netology/controller/PostController.java
@@ -9,33 +9,40 @@ import java.io.IOException;
 import java.io.Reader;
 
 public class PostController {
-  public static final String APPLICATION_JSON = "application/json";
-  private final PostService service;
+    public static final String APPLICATION_JSON = "application/json";
+    private final PostService service;
+    private final Gson gson = new Gson();
 
-  public PostController(PostService service) {
-    this.service = service;
-  }
+    public PostController(PostService service) {
+        this.service = service;
+    }
 
-  public void all(HttpServletResponse response) throws IOException {
-    response.setContentType(APPLICATION_JSON);
-    final var data = service.all();
-    final var gson = new Gson();
-    response.getWriter().print(gson.toJson(data));
-  }
+    public void all(HttpServletResponse response) {
+        deserializeSerialize(response, service.all());
+    }
 
-  public void getById(long id, HttpServletResponse response) {
-    // TODO: deserialize request & serialize response
-  }
+    public void getById(long id, HttpServletResponse response) {
+        // TODO: deserialize request & serialize response
+        deserializeSerialize(response, service.getById(id));
+    }
 
-  public void save(Reader body, HttpServletResponse response) throws IOException {
-    response.setContentType(APPLICATION_JSON);
-    final var gson = new Gson();
-    final var post = gson.fromJson(body, Post.class);
-    final var data = service.save(post);
-    response.getWriter().print(gson.toJson(data));
-  }
+    public void save(Reader body, HttpServletResponse response) {
+        final var post = gson.fromJson(body, Post.class);
+        deserializeSerialize(response, service.save(post));
+    }
 
-  public void removeById(long id, HttpServletResponse response) {
-    // TODO: deserialize request & serialize response
-  }
+    public void removeById(long id, HttpServletResponse response) {
+        // TODO: deserialize request & serialize response
+        service.removeById(id);
+        deserializeSerialize(response, "Post " + id + " has been deleted");
+    }
+
+    public <T> void deserializeSerialize(HttpServletResponse response, T data) {
+        response.setContentType(APPLICATION_JSON);
+        try {
+            response.getWriter().print(gson.toJson(data));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
